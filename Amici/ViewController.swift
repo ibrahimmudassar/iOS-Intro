22dc63300ccc233d0543d440085a8e9f17b80201
Ibrahim <22484328+ibrahimmudassar@users.noreply.github.com>
2024-02-03 22:40:00 -0500

    Initial Commit

---
+++ Amici/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Amici
+//
+//  Created by Ibrahim Mudassar on 2/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
+    
+    func changeColor() -> UIColor {
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+        
+        return UIColor(red: red, green: green, blue: blue,alpha: 0.5)
+    }
+}
